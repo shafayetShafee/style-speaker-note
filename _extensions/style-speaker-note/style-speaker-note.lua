95164c1a484e66bd6ebec3105458fb1e1a58cb7f
shafayetShafee <seaborn.sicario@gmail.com>
2023-01-26 01:54:35 +0600

    done with a functional filter and added examples

--- _extensions/style-speaker-note/style-speaker-note.lua
+++ _extensions/style-speaker-note/style-speaker-note.lua
@@ -1,6 +1,6 @@
-local p = quarto.log.output
 local str = pandoc.utils.stringify
 
+-- Inserts raw block of css style to each notes Div
 local function add_speaker_style(raw_block)
   local styler = {
     Div = function(el)
@@ -13,25 +13,27 @@ local function add_speaker_style(raw_block)
   return styler
 end
 
---if quarto.doc.isFormat("revealjs") then
 
-
-function Pandoc(doc)
-  local meta = doc.meta
-  if meta.speaker_notes_style then
-    --p(str(meta.speaker_notes_style))
-    local sh = io.open(str(meta.speaker_notes_style))
-    if not sh then
-      io.stderr:write("Cannot open file " .. meta.speaker_notes_style .. " | Skipping adding styles\n")
-    else
-      local styles = sh:read("a")
-      --p(styles)
-      sh:close()
-      local rb_str = "<style>" .. "\n\n" .. styles .. "\n\n" .. "</style>"
-      local raw_block = pandoc.RawBlock('html', rb_str)
-      --p(raw_block)
-      return doc:walk(add_speaker_style(raw_block))
+if quarto.doc.is_format('revealjs') then
+  function Pandoc(doc)
+    local meta = doc.meta
+    local fpath = str(meta.speaker_notes_style)
+    if meta.speaker_notes_style then
+      -- file handler for css file
+      local sh = io.open(fpath)
+      if not sh then
+        io.stderr:write("Cannot open file " .. fpath .. ".. Skipping adding styles\n")
+      else
+        -- read all of the contents
+        local styles = sh:read("a")
+        sh:close()
+        -- creating the raw block
+        local rb_str = "<style>" .. "\n\n" .. styles .. "\n\n" .. "</style>"
+        local raw_block = pandoc.RawBlock('html', rb_str)
+        -- inserting the style raw blocks to each notes div
+        return doc:walk(add_speaker_style(raw_block))
+      end
     end
   end
-end 
+end
   
